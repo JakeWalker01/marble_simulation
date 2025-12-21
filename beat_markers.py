66c7e7a474a14b2ea68d1b96662b09658f409808
Jake Walker <jwewalker@gmail.com>
2025-12-21 21:21:03 +0000

    Added threshold for beat detection

--- beat_markers.py
+++ beat_markers.py
@@ -18,6 +18,13 @@ prev = obj.scale.x
 scene.frame_set(start + 1)
 current = obj.scale.x
 
+
+def threshold_value(current, prev, threshold=0.2):
+    if abs(current - prev) >= threshold:
+        return True
+    else:
+        return False
+
 for frame in range(start + 1, end):
 
     scene.frame_set(frame - 1)
@@ -30,5 +37,16 @@ for frame in range(start + 1, end):
     next_val = obj.scale.x
 
     if current > prev and current > next_val:
-        print("Peak at frame", frame)
+        if threshold_value(current, prev):
+            print("beat at ", frame)
+
+
+"""
+Mostly works, but has issues.
+Example: around frame 70 the curve has two small beats close together.
+The first one gets detected because the value keeps rising,
+but the second one doesn’t because the change isn’t steep enough /
+it never becomes a clear peak.
+Basically this method struggles with small or clustered beats.
+"""
 
